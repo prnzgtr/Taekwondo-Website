3b12eb5f826849ba7c4b343f8ad8d1a569e97b1d
Infectynus <keyencechris@gmail.com>
2025-10-12 23:35:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+/* ---------- SCHEDULE DATA (edit this array to change the weekly schedule) ---------- */
+const scheduleData = [
+  { day: "Monday", time: "4:00 - 6:00 PM", className: "Kids" },
+  { day: "Monday", time: "6:00 - 8:00 PM", className: "Adults" },
+  { day: "Tuesday", time: "4:00 - 6:00 PM", className: "Kids" },
+  { day: "Tuesday", time: "6:00 - 8:00 PM", className: "Adults" },
+  { day: "Wednesday", time: "4:00 - 6:00 PM", className: "Kids" },
+  { day: "Wednesday", time: "6:00 - 8:00 PM", className: "Adults" },
+  { day: "Thursday", time: "4:00 - 6:00 PM", className: "Kids" },
+  { day: "Thursday", time: "6:00 - 8:00 PM", className: "Adults" },
+  { day: "Friday", time: "4:00 - 7:00 PM", className: "Kids & Adults" },
+];
 
+/* populate schedule table */
+const scheduleBody = document.getElementById("schedule-body");
+scheduleBody.innerHTML = scheduleData.map(s => `
+  <tr>
+    <td>${s.day}</td>
+    <td>${s.time}</td>
+    <td>${s.className}</td>
+  </tr>
+`).join("");
+/* achievements track animation control */
+  const track = document.querySelector(".achievements-track");
+  // Example: Reverse direction
+  function reverseDirection() {
+    track.style.animationDirection =
+      track.style.animationDirection === "reverse" ? "normal" : "reverse";
+  }
